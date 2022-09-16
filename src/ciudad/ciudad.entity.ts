9e996b661ccf423ce2a478bbd6d126a42f4c506d
mgaitans <m.gaitans@uniandes.edu.co>
2022-09-16 18:44:54 -0500

    Contenido de la clase del recurso Ciudad y Supermercado

--- src/ciudad/ciudad.entity.ts
+++ src/ciudad/ciudad.entity.ts
@@ -1,1 +1,26 @@
-export class CiudadEntity {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum Pais {
+    ARGENTINA = "Argentina",
+    ECUADOR   = "Ecuador",
+    PARAGUAY  = "Paraguay"
+}
+
+@Entity()
+export class CiudadEntity {
+ @PrimaryGeneratedColumn('uuid')
+ id: string;
+
+ @Column()
+ nombre: string;
+ 
+ @Column({
+    type: 'simple-enum',
+    enum: Pais
+  })
+  pais: Pais
+
+ @Column()
+ habitantes: string;
+
+}
--- src/supermercado/supermercado.entity.ts
+++ src/supermercado/supermercado.entity.ts
@@ -1,1 +1,19 @@
-export class SupermercadoEntity {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class SupermercadoEntity {
+ @PrimaryGeneratedColumn('uuid')
+ id: string;
+
+ @Column()
+ name: string;
+ 
+ @Column()
+ longitud: string;
+ 
+ @Column()
+ latitud : string;
+ 
+ @Column()
+ paginaweb: string;
+}
