3c32ed146a143b4cc4c7fc88e3662bd42e26ce22
mgaitans <m.gaitans@uniandes.edu.co>
2022-09-16 18:21:44 -0500

    creando el módulo Ciudad

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CiudadModule } from './ciudad/ciudad.module';
 
 @Module({
-  imports: [],
+  imports: [CiudadModule],
   controllers: [AppController],
   providers: [AppService],
 })
