7b14933458288c41d09d9ec97ff87c7a91bb66b5
mgaitans <m.gaitans@uniandes.edu.co>
2022-09-16 18:23:32 -0500

    creando el módulo Supermercado

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CiudadModule } from './ciudad/ciudad.module';
+import { SupermercadoModule } from './supermercado/supermercado.module';
 
 @Module({
-  imports: [CiudadModule],
+  imports: [CiudadModule, SupermercadoModule],
   controllers: [AppController],
   providers: [AppService],
 })
