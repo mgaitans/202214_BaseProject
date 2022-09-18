febb8348db11765d7cb185282f39f85951f9b757
mgaitans <m.gaitans@uniandes.edu.co>
2022-09-18 14:46:11 -0500

    Punto 3. API Se crea la clase del controlador para Ciudad-Supermercado

--- src/ciudad-supermercado/ciudad-supermercado.controller.ts
+++ src/ciudad-supermercado/ciudad-supermercado.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer'; 
+import { CiudadDto } from '../ciudad/ciudad.dto';
+import { CiudadEntity } from '../ciudad/ciudad.entity';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
+import { CiudadSupermercadoService } from './ciudad-supermercado.service';
 
 @Controller('ciudad-supermercado')
-export class CiudadSupermercadoController {}
+@UseInterceptors(BusinessErrorsInterceptor)
+export class CiudadSupermercadoController {
+    constructor(private readonly CiudadSupermercadoService: CiudadSupermercadoService){}
+    
+    @Post(':supermercadoId/cities/:ciudadId')
+    async addCiudadSupermercado(@Param('supermercadoId') supermercadoId: string, @Param('ciudadId') ciudadId: string){
+       return await this.CiudadSupermercadoService.addCiudadSupermercado(supermercadoId, ciudadId);
+    }
+    
+    @Get(':supermercadoId/cities/:ciudadId')
+    async findCiudadBySupermercadoIdCiudadId(@Param('supermercadoId') supermercadoId: string, @Param('ciudadId')ciudadId: string){
+        return await this.CiudadSupermercadoService.findCiudadBySupermercadoIdCiudadId(supermercadoId,ciudadId);
+    }
+    
+    @Put(':supermercadoId/cities')
+    async associateCiudadSupermercado(@Body() ciudadsDto: CiudadDto[], @Param('supermercadoId') supermercadoId: string){
+        const ciudads = plainToInstance(CiudadEntity, ciudadsDto)
+        return await this.CiudadSupermercadoService.associateCiudadSupermercado(supermercadoId, ciudads);
+    }
+    
+    @Delete(':supermercadoId/cities/:ciudadId')
+    @HttpCode(204)
+       async deleteCiudadSupermercado(@Param('supermercadoId') supermercadoId: string, @Param('ciudadId') ciudadId: string){
+           return await this.CiudadSupermercadoService.deleteCiudadSupermercado(supermercadoId, ciudadId);
+       }
+}
